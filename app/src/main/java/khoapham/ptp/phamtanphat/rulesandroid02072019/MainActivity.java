d242767fa5bcbe98714fd963cd493b1500ec5b9f
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-18 19:07:02 +0700

    insochan

--- app/src/main/java/khoapham/ptp/phamtanphat/rulesandroid02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/rulesandroid02072019/MainActivity.java
@@ -12,14 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // phuong thuc :
 //            1 : Pham vi hoat dong(access modifer)
 //            2 : Giá trị trả về cho phương thức(void cho kiểu dữ không trả về)
 //            3 : Khi đặt tên phương thức không nên viết hoa cho hành động
-
-
+        tinhsoChan(1000);
     }
-
+    private void tinhsoChan(int size){
+        for (int i = 0 ; i< size ; i++){
+            if (i % 2 == 0){
+                Log.d("BBB", String.valueOf(i));
+            }
+        }
+    }
 
 }
